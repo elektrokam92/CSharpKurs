01e3f2fe13a77db738be4704abeaa99d68d40e30
Kamil Marciniak <kamil.marciniak1992@gmail.com>
2018-10-23 22:38:19 +0200

    Kod z 20minuty 6 lekcji kursu

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Hello
+namespace Hello_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Wpisz swoje imię:  ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("witaj, " + name);
+
+            Console.Write("Wpisz ile masz lat:  ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+                if ((age < 18) & (age > 0))
+                {
+                    Console.WriteLine("Proszę mleko");
+                }
+                else if (result == false)
+                {
+                    Console.WriteLine("Wpisałeś niepoprawny wiek, popraw to!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Proszę piwo");
+                }
+
+            Console.ReadKey();
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj, " + item);
+            //}
         }
     }
 }
