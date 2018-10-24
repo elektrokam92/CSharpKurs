ad5069bf7edfef07eb4c2e62793a7f078c49a476
Kamil Marciniak <kamil.marciniak1992@gmail.com>
2018-10-24 21:59:16 +0200

    Kod napisany po zakończeniu ćwiczenia 6. Robienie własnych metod.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,34 +6,79 @@ namespace Hello_
     {
         static void Main(string[] args)
         {
-            Console.Write("Wpisz swoje imię:  ");
-            string name = Console.ReadLine();
+            ValueTypes();
 
-            Console.WriteLine("witaj, " + name);
+            for (; ; )
+            {
+                Greeting();
 
-            Console.Write("Wpisz ile masz lat:  ");
-            int age;
-            bool result = int.TryParse(Console.ReadLine(), out age);
+                Age();
+                Settings();
+            }
 
-                if ((age < 18) & (age > 0))
-                {
-                    Console.WriteLine("Proszę mleko");
-                }
-                else if (result == false)
-                {
-                    Console.WriteLine("Wpisałeś niepoprawny wiek, popraw to!!!");
-                }
-                else
-                {
-                    Console.WriteLine("Proszę piwo");
-                }
-
-            Console.ReadKey();
 
             //foreach (var item in args)
             //{
             //    Console.WriteLine("Witaj, " + item);
             //}
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("maxInt = " + maxInt);
+            Console.WriteLine("minInt = " + minInt);
+            Console.WriteLine("maxLong = " + maxLong);
+            Console.WriteLine("minLong = " + minLong);
+        }
+
+        /// <summary>
+        /// Reset ustawień do domyślnych
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Wypisujemy komunikat zależny od wieku
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Wpisz ile masz lat:  ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if ((age < 18) & (age > 0))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Proszę mleko");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wpisałeś niepoprawny wiek, popraw to!!!");
+            }
+            else
+            {
+                Console.WriteLine("Proszę piwo");
+            }
+        }
+
+        /// <summary>
+        /// Wypisujemy powitanie użytkownika
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("Wpisz swoje imię:  ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("witaj, " + name);
         }
     }
 }
