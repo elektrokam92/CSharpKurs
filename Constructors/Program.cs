84f8f5e49b6ab6a9a61983c42c93427b113647f3
Kamil Marciniak <kamil.marciniak1992@gmail.com>
2018-11-14 22:34:57 +0100

    Konstruktor. Program naspiany po lekcji nr 9.

---
+++ Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person person2 = new Person(1, "Kamil");
+            Person person3 = new Person(2, "Kamil", "Marciniak");
+            Person person4 = new Person(2, "Kamil", "Marciniak", "Katowice", 19, "mężczyzna");
+                                          
+        }
+    }
+}
